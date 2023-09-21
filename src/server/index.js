346d847cf1e295a4085e2613541097f0be9b4dc9
Eduardo <ehernandezvilla@gmail.com>
2023-09-21 12:52:33 -0300

    general changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,61 +1,50 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var bodyParser = require('body-parser')
-var cors = require('cors')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
-
-// var json = {
-//     'title': 'test json response',
-//     'message': 'this is a message',
-//     'time': 'now'
-// }
-
-const app = express()
+const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200
+    origin: ['http://localhost:8080', 'https://example-api-express.onrender.com'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-app.use(cors(corsOptions)); //Cors with options
 
-// app.use(cors);
+app.use(cors(corsOptions));
 
-// to use json
-app.use(bodyParser.json())
-// to use url encoded values
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-
-app.use(express.static('dist'))
-
-// console.log(JSON.stringify(mockAPIResponse))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+    res.sendFile('dist/index.html');
+});
 
 app.get('/test', function (req, res) {
     res.json(mockAPIResponse);
-})
-
-app.post('/nameapi', async (req, res) => {
-    const response = await fetch(`https://api.nameapi.org/rest/v5.3/parser/personnameparser?apiKey=${process.env.API_KEY}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req.body)
-    });
-    const data = await response.json();
-    res.json(data);
 });
 
+app.post('/nameapi', async (req, res) => {
+    try {
+        const response = await fetch(`https://api.nameapi.org/rest/v5.3/parser/personnameparser?apiKey=${process.env.API_KEY}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(req.body)
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+app.listen(8081, '0.0.0.0', function () {
+    console.log('Example app listening on port 8081!');
+});
+
